637b1bc2bed75e6c5edba91d4d051891876a6d3f
Antonio <antonioru1997@gmail.com>
2021-07-30 16:32:03 +0200

    FINE ESERCIZIO

--- js/main.js
+++ js/main.js
@@ -17,3 +17,67 @@ console.log("INFORMAZIONI PRIMO STUDENTE SINGOLO.")
 for (let key in studente) {
     console.log(key + " : " + studente[key]);
 }
+
+//Creo un array di oggetti di studenti.
+let studenti = [
+    {
+        nome : "Harry",
+        cognome : "Potter",
+        eta : 25
+    },
+    {
+        nome : "Hermione",
+        cognome : "Granger",
+        eta : 26
+    },
+    {
+        nome : "Ronald",
+        cognome : "Weasley",
+        eta : 25
+    },
+    {
+        nome : "Albus Percival Wulfric Brian",
+        cognome : "Silente",
+        eta : 115
+    }
+]
+
+//"pusho" il primo studente inserito in precedenza
+studenti.push(studente);
+
+// richiamo funzione per chiedere i dati all'utente
+userStudent();
+
+// richiamo funzione per inserire l'oggetto nell'array "studenti"
+putInArray(studenti, userInfo);
+
+//Stampo le proprietà di tutti gli studenti dell'array
+console.log("INFORMAZIONI ARRAY STUDENTI.")
+for (let i = 0; i < studenti.length; i++) {
+    for (let key in studenti[i]) {
+        console.log(key + " : " + studenti[i][key])
+    }
+}
+
+/* -----FUNZIONI----- */
+
+/* Funzione che:
+    - Chiede all'utente i dati tramite prompt
+    - Inserisce questi ultimi in un oggetto
+*/
+function userStudent() {
+    //Chiedo i dati
+    let userName = prompt("Inserisci il nome");
+    let userSurname = prompt("Inserisci il cognome");
+    let userEta = parseInt(prompt("Inserisci l'età"));
+    //Creo oggetto per l'utente
+    userInfo = {};
+    userInfo.nome = userName;
+    userInfo.cognome = userSurname;
+    userInfo.eta = userEta;
+}
+
+// Funzione per inserire un oggetto in un array.
+function putInArray (array, object) {
+    array.push(object);
+}
