a406b1d56b8a9ffba1c5e81c6a0c476e58fa9ec4
Antonio <antonioru1997@gmail.com>
2021-07-30 15:57:11 +0200

    Creo oggetto di uno studente

---
+++ js/main.js
@@ -0,0 +1,19 @@
+/* Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
+Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
+Creare un array di oggetti di studenti.
+Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+Dare la possibilità all’utente, attraverso 3 prompt(), di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
+
+
+//Creo un oggetto che descriva uno studente
+let studente = {
+    nome : "Antonio",
+    cognome : "Russo",
+    eta : 24
+}
+
+//Stampo le proprietà
+console.log("INFORMAZIONI PRIMO STUDENTE SINGOLO.")
+for (let key in studente) {
+    console.log(key + " : " + studente[key]);
+}
